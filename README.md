97c68e50af2b0a9d70166b1263d5cb8cea37dbbf
Mike Taykle <miketaykle@o5p.me>
2021-12-13 03:43:14 -0500

    update

---
+++ README.md
@@ -0,0 +1,3 @@
+# WordPress Theme: Skeleton
+
+This is a skeleton directory for new WP Groove WordPress themes.
---
+++ trunk/src/classes/Theme.php
@@ -0,0 +1,44 @@
+<?php
+/** WP Groove™ <https://wpgroove.com>
+ *  _       _  ___       ___
+ * ( )  _  ( )(  _`\    (  _`\
+ * | | ( ) | || |_) )   | ( (_) _ __   _      _    _   _    __  ™
+ * | | | | | || ,__/'   | |___ ( '__)/'_`\  /'_`\ ( ) ( ) /'__`\
+ * | (_/ \_) || |       | (_, )| |  ( (_) )( (_) )| \_/ |(  ___/
+ * `\___x___/'(_)       (____/'(_)  `\___/'`\___/'`\___/'`\____)
+ */
+namespace WP_Groove\Skeleton_Theme;
+
+/**
+ * Dependencies.
+ *
+ * @since 1.0.0
+ */
+use Clever_Canyon\Utilities\OOPs\Version_1_0_0 as U;
+use WP_Groove\Framework\Utilities\OOPs\Version_1_0_0 as UU;
+use WP_Groove\Framework\Theme\Version_1_0_0\{ Base };
+
+/**
+ * Theme.
+ *
+ * @since 1.0.0
+ */
+class Theme extends Base {
+	/**
+	 * On `after_setup_theme` hook.
+	 *
+	 * @since 1.0.0
+	 */
+	public function on_after_setup_theme() : void {
+		// Nothing for now.
+	}
+
+	/**
+	 * On `init` hook.
+	 *
+	 * @since 1.0.0
+	 */
+	public function on_init() : void {
+		// Nothing for now.
+	}
+}
